c2631fa63828745d9e3c8871c20d539cff318981
Sibhat Temelso <sibhat.temelso@gmail.com>
2019-02-15 13:59:47 -0800

    generalize serverjs

--- api/index.js
+++ api/index.js
@@ -3,38 +3,5 @@ let server = express.Router();
 
 const queries = require("../data/queries");
 
-server.get("/", (req, res) =>{
-    queries.getProductsAll().then(result => {
-        // console.log("result is", {result});
-        res.status(200);
-        res.json(result)
-    }).catch(error => {
-        res.status(500);
-        res.json({"error":error});
-    })
-
-});
-server.get("/category", (req, res) =>{
-    queries.getCatagoryAll().then(result => {
-        // console.log("result is", {result});
-        res.status(200);
-        res.json(result)
-    }).catch(error => {
-        res.status(500);
-        res.json({"error":error});
-    })
-
-});
-server.get("/department", (req, res) =>{
-    queries.getDepartmentAll().then(result => {
-        // console.log("result is", {result});
-        res.status(200);
-        res.json(result)
-    }).catch(error => {
-        res.status(500);
-        res.json({"error":error});
-    })
-
-});
 server.use("/users", queries.getAllUsers);
 module.exports = server;
